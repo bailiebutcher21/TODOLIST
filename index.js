f45c2d9b7e27f0db1cd489709de89060fff14010
bailiebutcher <bailiebutcher@gmail.com>
2017-11-07 10:17:07 -0700

    Created Checkbox Next to input. Started Styling delete box.

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 
-    $("#inputbox").keyup(function(event){
-        var input = $("#inputbox").val();
-        if(event.keyCode ===13){
-            addItem(input);
-        }
-    });
+$("#inputbox").keyup(function(event){
+    var input = $("#inputbox").val();
+    if(event.keyCode ===13){
+        addItem(input);
+    }
+});
 
 function addItem(input) {
     var thelabel = $("#inputbox").val();
+
     if (thelabel !== "") {
-        var inputdiv = "<div><span onclick='editItem(this)'>" + thelabel + "</span><button id=deletebutton onclick='deleteItem(this)'>Delete</button></div>";
+        var inputdiv = "<div><input type='checkbox' onclick='markComplete()'><span onclick='editItem(this)'>" + thelabel + "</span><button class=deletebutton id=deletebutton onclick='deleteItem(this)'>X</button></div>";
 
         $("#unfinished-tasks").append(inputdiv);
         $("#inputbox").val("");
+
+      //  $("#source").appendTo("#markComplete");
     }
 }
+var markComplete = function (element){
+ var inputdiv = "<div><button id=deletebutton onclick='deleteItem(this)'>X</button></div>";
 
-    var editItem = function(element) {
-        var text = element.innerText;
-        var input ="<input onkeyup='save(this)' value='"+ text +"'>";
-        $(element).parent().prepend(input);
-        $(element).remove();
 
-    };
+};
+var editItem = function(element) {
+    var text = element.innerText;
+    var input ="<input onkeyup='save(this)' value='"+ text +"'>";
+    $(element).parent().prepend(input);
+    $(element).remove();
+
+};
 function save(element){
     if(event.keyCode ===13){
         var text = element.value;
@@ -34,6 +41,7 @@ function save(element){
 function deleteItem(element){
     $(element).parent().remove();
 }
+
 
 
 
